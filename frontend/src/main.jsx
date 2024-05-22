e5970a84668f60dd14e60a2258b42054741cebc7
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-05-21 22:09:17 -0500

    Commit init - Estructura del Proyecto

---
+++ frontend/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './index.css';
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registrado con éxito:", registration);
+      })
+      .catch((error) => {
+        console.log("Error al registrar el Service Worker:", error);
+      });
+  });
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
---
+++ frontend/src/service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", (event) => {
+  console.log("Service worker installing...");
+  // Añade archivos al cache
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service worker activating...");
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("Fetching:", event.request.url);
+  // Puedes añadir lógica para servir archivos del cache
+});
