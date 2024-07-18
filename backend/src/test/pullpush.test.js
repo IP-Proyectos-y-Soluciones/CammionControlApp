0060008f51b8a19eba0d612b009dcfd66fbee4b1
ManuelFBS <manuelf.borrego@gmail.com>
2024-07-18 12:40:00 -0400

    Una prueba más...

--- backend/src/test/pullpush.test.js
+++ backend/src/test/pullpush.test.js
@@ -2,4 +2,6 @@ export function testPullMerge() {
   console.log(
     "Esta es solo código para probar el pull-merge con la branch 'dev' del proyecto...!",
   );
+
+  let varCualquiera = 'Prueba';
 }
