cb9798379ee333b08d8c2ca3e5ab6ec56b9212c2
ManuelFBS <manuelf.borrego@gmail.com>
2024-08-02 15:01:24 -0400

    Correción y modificación en auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { login } from '../authentication/login_out/login.controller';
 import { logout } from '../authentication/login_out/logout.controller';
+import { TokenValidation } from '../authentication/tokens/verifyToken';
 
 const router = Router();
 
 router.post('/login', login);
 
 router.post('/logout', logout);
+
+router.get('/check', TokenValidation, (req, res) => {
+  res.json({ isAuthenticated: true });
+});
 
 export default router;
