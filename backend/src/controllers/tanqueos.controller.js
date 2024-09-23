ce4c997f5ae6a936f30620e4f20c75d694d894ff
ManuelFBS <manuelf.borrego@gmail.com>
2024-09-23 02:33:29 -0400

    Modificaciones en: login.controller.js, tanqueos.controller.js y sus respectivas routes...

--- backend/src/controllers/tanqueos.controller.js
+++ backend/src/controllers/tanqueos.controller.js
@@ -1,199 +1,199 @@
 // importar los modelos con import
-import Tanqueo from '../models/Tanqueo';
-import Persona from '../models/Persona';
-import Vehiculo from '../models/Vehiculo';
-import { findVehicleAndDriver } from '../libs/FindVehicleAndDriver';
+import Tanqueo from "../models/Tanqueo";
+import Persona from "../models/Persona";
+import Vehiculo from "../models/Vehiculo";
+import { findVehicleAndDriver } from "../libs/FindVehicleAndDriver";
 
 // Crear un nuevo tanqueo
 export const createTanqueo = async (req, res) => {
-    try {
-        const {
-            fecha_tanqueo,
-            n_recibo,
-            estacion,
-            cantidad_galones,
-            valor_tanqueo,
-            placas,
-            cedula,
-        } = req.body;
+  try {
+    const {
+      fecha_tanqueo,
+      n_recibo,
+      estacion,
+      cantidad_galones,
+      valor_tanqueo,
+      placas,
+      cedula,
+    } = req.body;
 
-        const queryResult = await findVehicleAndDriver(placas, cedula);
+    const queryResult = await findVehicleAndDriver(placas, cedula);
 
-        console.log(queryResult.found);
+    console.log(queryResult.found);
 
-        if (!queryResult.found) {
-            return res.status(404).json({ message: queryResult.message });
-        }
-
-        const refuelingData = new Tanqueo({
-            fecha_tanqueo,
-            n_recibo,
-            estacion,
-            cantidad_galones,
-            valor_tanqueo,
-            vehiculo_placa: placas,
-            vehiculo: queryResult.vehicleId,
-            conductor_cedula: cedula,
-            conductor: queryResult.driverId,
-        });
-
-        const newRefueling = await refuelingData.save();
-
-        await Persona.findByIdAndUpdate(
-            queryResult.driverId,
-            { $push: { tanqueos: newRefueling._id } },
-            { new: true },
-        );
-
-        await Vehiculo.findByIdAndUpdate(
-            queryResult.vehicleId,
-            { $push: { tanqueos: newRefueling._id } },
-            { new: true },
-        );
-
-        return res.status(201).json({
-            message: 'El formulario se ha registrado correctamente...!',
-            newRefueling,
-        });
-    } catch (error) {
-        if (error instanceof Error) {
-            return res.status(500).json({ error: error.message });
-        } else {
-            return res.status(500).json(error);
-        }
+    if (!queryResult.found) {
+      return res.status(404).json({ message: queryResult.message });
     }
+
+    const refuelingData = new Tanqueo({
+      fecha_tanqueo,
+      n_recibo,
+      estacion,
+      cantidad_galones,
+      valor_tanqueo,
+      vehiculo_placa: placas,
+      vehiculo: queryResult.vehicleId,
+      conductor_cedula: cedula,
+      conductor: queryResult.driverId,
+    });
+
+    const newRefueling = await refuelingData.save();
+
+    await Persona.findByIdAndUpdate(
+      queryResult.driverId,
+      { $push: { tanqueos: newRefueling._id } },
+      { new: true }
+    );
+
+    await Vehiculo.findByIdAndUpdate(
+      queryResult.vehicleId,
+      { $push: { tanqueos: newRefueling._id } },
+      { new: true }
+    );
+
+    return res.status(201).json({
+      message: "El formulario se ha registrado correctamente...!",
+      newRefueling,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ error: error.message });
+    } else {
+      return res.status(500).json(error);
+    }
+  }
 };
 
 // Obtener todos los tanqueos
 export const getAllTanqueos = async (req, res) => {
-    try {
-        const tanqueos = await Tanqueo.find();
-        res.status(200).json({
-            message: ' Tanqueo traidos exitosamente',
-            data: tanqueos,
-        });
-    } catch (error) {
-        res.status(500).json(error);
-    }
+  try {
+    const tanqueos = await Tanqueo.find();
+    res.status(200).json({
+      message: " Tanqueo traidos exitosamente",
+      data: tanqueos,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 // Obtener un tanqueo por ID
 export const getTanqueoById = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const tanqueo = await Tanqueo.findById(id);
-        if (!tanqueo) {
-            return res.status(404).json();
-        }
-        res.status(200).json({
-            message: ' Tanqueo encontrado exitosamente',
-            data: tanqueo,
-        });
-    } catch (error) {
-        res.status(500).json(error);
+  try {
+    const tanqueo = await Tanqueo.findById(id);
+    if (!tanqueo) {
+      return res.status(404).json();
     }
+    res.status(200).json({
+      message: " Tanqueo encontrado exitosamente",
+      data: tanqueo,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 // Actualizar un tanqueo por ID
 export const updateTanqueo = async (req, res) => {
-    const { id } = req.params;
-    const {
-        fecha_tanqueo,
-        n_recibo,
-        estacion,
-        cantidad_galones,
-        valor_tanqueo,
-        vehiculo,
-        conductor,
-    } = req.body;
-    const updates = {
-        fecha_tanqueo,
-        n_recibo,
-        estacion,
-        cantidad_galones,
-        valor_tanqueo,
-        vehiculo,
-        conductor,
-    };
+  const { id } = req.params;
+  const {
+    fecha_tanqueo,
+    n_recibo,
+    estacion,
+    cantidad_galones,
+    valor_tanqueo,
+    vehiculo,
+    conductor,
+  } = req.body;
+  const updates = {
+    fecha_tanqueo,
+    n_recibo,
+    estacion,
+    cantidad_galones,
+    valor_tanqueo,
+    vehiculo,
+    conductor,
+  };
 
-    // Filtrar las propiedades no definidas
-    Object.keys(updates).forEach(
-        (key) => updates[key] === undefined && delete updates[key],
-    );
+  // Filtrar las propiedades no definidas
+  Object.keys(updates).forEach(
+    (key) => updates[key] === undefined && delete updates[key]
+  );
 
-    try {
-        const tanqueo = await Tanqueo.findByIdAndUpdate(id, updates, {
-            new: true,
-            runValidators: true,
-        });
-        if (!tanqueo) {
-            return res.status(404).json();
-        }
-        res.status(200).json({
-            message: ' Tanqueo actualizado exitosamente',
-            data: tanqueo,
-        });
-    } catch (error) {
-        res.status(400).json(error);
+  try {
+    const tanqueo = await Tanqueo.findByIdAndUpdate(id, updates, {
+      new: true,
+      runValidators: true,
+    });
+    if (!tanqueo) {
+      return res.status(404).json();
     }
+    res.status(200).json({
+      message: " Tanqueo actualizado exitosamente",
+      data: tanqueo,
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 // Eliminar un tanqueo por ID
 export const deleteTanqueo = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const tanqueo = await Tanqueo.findByIdAndDelete(id);
-        if (!tanqueo) {
-            return res.status(404).send('id no encontrado');
-        }
-        res.status(200).json(id + ' eliminado con éxito');
-    } catch (error) {
-        res.status(500).json(error);
+  try {
+    const tanqueo = await Tanqueo.findByIdAndDelete(id);
+    if (!tanqueo) {
+      return res.status(404).send("id no encontrado");
     }
+    res.status(200).json(id + " eliminado con éxito");
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
-// ********** Funciones auxiliares para uso directo en el Front ********** //
+// // ********** Funciones auxiliares para uso directo en el Front ********** //
 
-export const getDriverByDNI = async (req, res) => {
-    try {
-        const { cedula } = req.params;
+// export const getDriverByDNI = async (req, res) => {
+//     try {
+//         const { cedula } = req.params;
 
-        const persona = await Persona.findOne({
-            cedula: cedula,
-        });
+//         const persona = await Persona.findOne({
+//             cedula: cedula,
+//         });
 
-        if (!persona)
-            return res
-                .status(404)
-                .json({ message: 'Persona no encontrada...!' });
+//         if (!persona)
+//             return res
+//                 .status(404)
+//                 .json({ message: 'Persona no encontrada...!' });
 
-        return res.status(200).json(persona);
-    } catch (error) {
-        if (error instanceof Error) {
-            return res.status(500).json({ error: error.message });
-        } else {
-            return res.status(500).json(error);
-        }
-    }
-};
+//         return res.status(200).json(persona);
+//     } catch (error) {
+//         if (error instanceof Error) {
+//             return res.status(500).json({ error: error.message });
+//         } else {
+//             return res.status(500).json(error);
+//         }
+//     }
+// };
 
-export const getVehicleById = async (req, res) => {
-    const { id } = req.params;
+// export const getVehicleById = async (req, res) => {
+//     const { id } = req.params;
 
-    try {
-        const vehiculo = await Vehiculo.findById(id);
-        if (!vehiculo) {
-            return res.status(404).send('ID no encontrado');
-        }
-        res.status(200).json({
-            message: 'Búsqueda por ID exitosa',
-            data: vehiculo,
-        });
-    } catch (error) {
-        res.status(500).json(error);
-    }
-};
+//     try {
+//         const vehiculo = await Vehiculo.findById(id);
+//         if (!vehiculo) {
+//             return res.status(404).send('ID no encontrado');
+//         }
+//         res.status(200).json({
+//             message: 'Búsqueda por ID exitosa',
+//             data: vehiculo,
+//         });
+//     } catch (error) {
+//         res.status(500).json(error);
+//     }
+// };
 
-// *********************************************************** //
+// // *********************************************************** //
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,26 +1,45 @@
-import { Router } from 'express';
-import { login } from '../authentication/login_out/login.controller';
-import { logout } from '../authentication/login_out/logout.controller';
+import { Router } from "express";
+import {
+  login,
+  getDriverByDNI,
+  getVehicleById,
+} from "../authentication/login_out/login.controller";
+import { logout } from "../authentication/login_out/logout.controller";
 // import { TokenValidation } from '../authentication/tokens/verifyToken'; // Activar para la producción...
-import { AuxAuthMiddleware } from '../middlewares/auxAuthMiddleware'; // Eliminar para prodcc...
+import { AuxAuthMiddleware } from "../middlewares/auxAuthMiddleware"; // Eliminar para prodcc...
 
 const router = Router();
 
-router.post('/login', login);
+router.post("/login", login);
 
-router.post('/logout', logout);
+router.post("/logout", logout);
 
 // router.get('/check', TokenValidation, (req, res) => {
 //   res.json({ isAuthenticated: true });
 // });
 
-router.get('/checklogin', AuxAuthMiddleware, (req, res) => {
-    res.json({
-        isAuthenticated: true,
-        user: {
-            roles: req.session.roles,
-        },
-    });
+router.get("/checklogin", AuxAuthMiddleware, (req, res) => {
+  res.json({
+    isAuthenticated: true,
+    user: {
+      roles: req.session.roles,
+    },
+  });
 });
+
+// *********** || Rutas auxiliares para el Front... || ************ //
+
+router.get(
+  "/driverced/:cedula",
+  // TokenValidation,
+  getDriverByDNI
+);
+router.get(
+  "/vehicleid/:id",
+  // TokenValidation,
+  getVehicleById
+);
+
+// ***************************************************** //
 
 export default router;
