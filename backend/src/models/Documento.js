b80a21529e62352dd5d1e0ba74f0d5c577c0597f
ManuelFBS <manuelf.borrego@gmail.com>
2024-09-24 08:35:47 -0400

    Actualización del modelo "documento.js".

--- backend/src/models/Documento.js
+++ backend/src/models/Documento.js
@@ -1,35 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const documentoSchema = new mongoose.Schema(
-    {
-        cerificado_N: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        tipo: {
-            type: String,
-            enum: ['Poliza de seguro', 'Soat', 'tecnomecanica'],
-            required: true,
-        },
-        vehiculo_placa: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        fecha_expedicion: Date,
-        fecha_vencimiento: Date,
-        vehiculo: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Vehiculo',
-        },
+  {
+    cerificado_N: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    {
-        timestamps: false,
-        autoCreate: false,
+    tipo: {
+      type: String,
+      enum: ["Póliza de seguro", "Soat", "Tecnomecánica"],
+      required: true,
     },
+    vehiculo_placa: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fecha_expedicion: Date,
+    fecha_vencimiento: Date,
+    vehiculo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vehiculo",
+    },
+  },
+  {
+    timestamps: false,
+    autoCreate: false,
+  }
 );
 
-const Documento = mongoose.model('Documento', documentoSchema);
+const Documento = mongoose.model("Documento", documentoSchema);
 
 export default Documento;
