1115926574f82e9dfcecdeb9acae176fcd4195f4
ManuelFBS <manuelf.borrego@gmail.com>
2024-10-20 18:42:12 -0400

    Actualización de la carpeta "/dist"...

--- backend/dist/routes/volqueta.routes.js
+++ backend/dist/routes/volqueta.routes.js
@@ -6,9 +6,18 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 var _express = require("express");
 var _volquetas = require("../controllers/volquetas.controller");
+var _GenRandomControlNumb = require("../libs/GenRandomControlNumb");
 // import { TokenValidation } from '../authentication/tokens/verifyToken'; // Activar para la producción...
 
 var router = (0, _express.Router)();
+router.get('/gennumber',
+// TokenValidation,
+function (req, res) {
+  var numbOfForm = (0, _GenRandomControlNumb.generarNumeroPlanilla)();
+  res.json({
+    numbOfForm: numbOfForm
+  });
+});
 router.post('/addplanilla',
 // TokenValidation,
 _volquetas.createVolqueta);
